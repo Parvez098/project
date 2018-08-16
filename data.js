55a17260a2a963225d33f862d2bd066d9c7cdc54
Arun Kumar <exceltes@gmail.com>
2018-08-16 12:21:15 +0530

    Changes done till now

---
+++ data.js
@@ -0,0 +1,9 @@
+const items = [
+    {user: {firstName:"sonu",lastName:"ahmed",email:"sonu@gmmail.com",password:'123'}, profile: {dob:"04-07-98",mobile_no:"10101010"}},
+    {user: {firstName:"a",lastName:"b",email:"a@gmmail.com",password:'123'}, profile: {dob:"04-07-98",mobile_no:"10101010"}},
+    {user: {firstName:"c",lastName:"d",email:"c@gmmail.com",password:'123'},  profile: {dob:"04-07-98",mobile_no:"10101010"}},
+    {user: {firstName:"d",lastName:"e",email:"d@gmmail.com",password:'123'},  profile: {dob:"04-07-98",mobile_no:"10101010"}},
+    {user: {firstName:"n",lastName:"m",email:"n@gmmail.com",password:'123'},  profile: {dob:"04-07-98",mobile_no:"10101010"}}
+    ];
+
+module.exports.items = items;
